249c0bc22715ee0e600d868d023bd4a4a00f61d5
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-08-27 14:29:08 -0700

    create start funct display entry terminal options

--- cli.py
+++ cli.py
@@ -1,3 +1,4 @@
+from simple_term_menu import TerminalMenu
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -8,7 +9,26 @@ session = Session()
 
 
 class Cli():
-    pass
+    def __init__(self):
+        self.current_user = None
+        self.current_makeup = None
+        self.current_makeup_product = None
+
+    # START PROGRAM
+    def start(self):
+        options = ["Login", "Sign Up", "Exit"]
+        terminal_menu = TerminalMenu(options)
+        menu_index = terminal_menu.show()
+
+        if options[menu_index] == "Login":
+            self.handle_login()
+        elif options[menu_index] == "Sign Up":
+            self.handle_signup()
+        else:
+            self.handle_exit()
+
+
+
 
 app = Cli()
 app.start()
