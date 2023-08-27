637ed9ad9afdfd1c6b76614652bbe5475d37bea7
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-08-27 13:18:49 -0700

    create Makeup class and makeups table

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import (Column, String, Integer, Column)
+from sqlalchemy import (Column, String, Integer, Column, Index)
 from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
@@ -9,3 +9,18 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     username = Column(String, unique=True)
+
+
+
+
+
+class Makeup(Base):
+    __tablename__ = "makeups"
+    Index('index_name', 'name')
+    Index('index_brand', 'brand')
+    Index('index_product_type', 'product_type')
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    brand = Column(String)
+    product_type = Column(String)
