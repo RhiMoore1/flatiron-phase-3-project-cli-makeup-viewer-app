77b7bb6bb4b53ba31e70d022958bda0670006fcf
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-08-27 13:38:18 -0700

    create engine for connection/session to speak to db

--- seeds.py
+++ seeds.py
@@ -1,1 +1,8 @@
 from models import User, Makeup, user_makeup_favorite
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///makeup_data.db')
+
+Session = sessionmaker(bind=engine)
+session = Session()
