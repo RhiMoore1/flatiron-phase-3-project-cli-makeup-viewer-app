215d7208faad9f81c9083685c92e8ff138db6bd5
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-08-27 13:17:49 -0700

    create a User class and users table

--- models.py
+++ models.py
@@ -1,2 +1,11 @@
+from sqlalchemy import (Column, String, Integer, Column)
 from sqlalchemy.orm import declarative_base
 Base = declarative_base()
+
+
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
