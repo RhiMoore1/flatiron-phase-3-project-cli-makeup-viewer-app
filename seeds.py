ed29df4aa6b1459715bddad7d1f42037799a1385
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-08-27 13:44:25 -0700

    use makeup API to generate makeup and seed makeups

--- seeds.py
+++ seeds.py
@@ -7,6 +7,8 @@ engine = create_engine('sqlite:///makeup_data.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
+import json 
+import requests 
 from faker import Faker
 fake = Faker()
 
@@ -25,3 +27,38 @@ for __ in range(5):
  
     session.add(new_user)
     session.commit()
+
+
+
+
+# Use the Makeup DB to populate the makeup by sending a request then parse as json
+request = requests.get(f'http://makeup-api.herokuapp.com/api/v1/products.json')
+makeup_data = json.loads(request.text)
+
+
+BRANDS = ['mistura', 'benefit', 'dr. hauschka', 'misa', 'nyx', 'butter london', 'dior', 
+          'clinique', 'rejuva minerals', 'mineral fusion', "l'oreal", 'moov', 'piggy paint', 
+          'e.l.f.', 'physicians formula', 'fenty', 'alva', "maia's mineral galaxy", 'colourpop', 
+          'annabelle', 'zorah', 'boosh', 'lotus cosmetics usa', 'marcelle', 'dalish', 'revlon', 
+          'china glaze', 'smashbox', 'salon perfect', 'maybelline', 'covergirl', 'orly', 
+          'milani', 'almay', 'deciem', 'green people', 'cargo cosmetics', 'anna sui', 'nudus', 
+          'pacifica', 'w3llpeople', 'penny lane organics', 'stila', 'sinful colours', "burt's bees", 
+          'sante', 'zorah biocosmetiques', 'glossier', 'coastal classic creation', 'pure anada', 
+          'essie', 'suncoat', "c'est moi", 'marienatie', 'iman', "sally b's skin yummies", 
+          'wet n wild']
+
+MAKEUP_API_SEARCH_URL_BASE = "http://makeup-api.herokuapp.com/api/v1/products.json?brand="
+
+makeup_product_ids = set()
+for brand in BRANDS:
+    response = requests.get(MAKEUP_API_SEARCH_URL_BASE + brand)
+    json_data = json.loads(response.text)
+    for makeup_product in json_data:
+        new_makeup_product = Makeup(
+            name = makeup_product["name"],
+            brand = makeup_product["brand"],
+            product_type = makeup_product["product_type"]
+        )
+
+        session.add(new_makeup_product)
+        session.commit()
