cae6dab49f4ed7414a06ae52562ec75aaca2f0e4
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-08-27 13:39:51 -0700

    use faker to seed users table

--- seeds.py
+++ seeds.py
@@ -6,3 +6,22 @@ engine = create_engine('sqlite:///makeup_data.db')
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+from faker import Faker
+fake = Faker()
+
+
+
+# Use faker to seed 5 Users
+for __ in range(5):
+    
+    user_first_name = fake.first_name_female()
+    user_last_name = fake.last_name()
+    user_full_name = user_first_name + user_last_name
+    
+    new_user = User(
+        username = user_full_name,
+    )
+ 
+    session.add(new_user)
+    session.commit()
