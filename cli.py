8941a8bd20165abad29a370fd028f96dab42603a
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-08-27 14:50:37 -0700

    create function and menu options to manage makeups

--- cli.py
+++ cli.py
@@ -42,6 +42,7 @@ class Cli():
             return
         else:
             self.current_user = user
+            self.handle_manage_makeups()
 
 
     # SIGNUP
@@ -61,11 +62,34 @@ class Cli():
                 session.commit()
                 print(f"Your Username is: {user.username}")
                 time.sleep(3)
+                self.handle_manage_makeups()
             else:
                 print("User already exists. Please log in or create a new user.")
                 time.sleep(3)
                 self.start()
  
+
+
+# MANAGE MAKEUP - ENTRY POINT 
+    def handle_manage_makeups(self):
+        options = ["View Makeups", "Create a Makeup", "Delete a Makeup", "Favorite a Makeup", "Exit"]
+        terminal_menu = TerminalMenu(options)
+        menu_index = terminal_menu.show()
+
+        if menu_index == 0:
+            print("View makeups")
+            self.handle_view_makeups()    
+        elif menu_index == 1:
+            print("Create a makeup")
+            self.handle_create_new_makeup()
+        elif menu_index == 2:
+            print("Delete a makeup")
+            self.handle_delete_a_makeup()
+        elif menu_index == 3:
+            print("Favorite a makeup")
+            self.handle_favorite_a_makeup()
+        else:
+            self.handle_exit()
 
 
 
